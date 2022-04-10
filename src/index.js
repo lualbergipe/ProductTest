143541011e43830a5134967082075a08b02335d0
Luis Giraldo <lualbergipe@gmail.com>
2022-04-10 12:05:12 -0500

    se edita el archivo index.js para quitar una importación

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min';
 ReactDOM.render(
   <React.StrictMode>
     <App />
